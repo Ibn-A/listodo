1ba38d92d529224c2d82cdff9c9e70f392167c2f
IA <IA@MacBook-Pro-de-Elsa.local>
2020-01-23 12:57:13 +0100

    Storing temporary UI data in component state : -Add a client-side data filtering feature to our app

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,6 +8,15 @@ import Task from './Task.js';
 
 //App component - represents the whole app
 class App extends Component {
+    // Add initial state to App component
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: false,
+        };
+    }
+
     handleSubmit(event){
         event.preventDefault();
 
@@ -22,9 +31,20 @@ class App extends Component {
         // Clear form
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
-
+    //add toggleHideCompleted handler to App
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
+    // Filter completed tasks in renderTasks
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+
+        }
+        return filteredTasks.map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -34,6 +54,16 @@ class App extends Component {
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+                    
+                    <label>
+                        <input
+                            type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)}
+                        />
+                        Hide Completed Tasks
+                    </label>
 
                     <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
                         <input 
