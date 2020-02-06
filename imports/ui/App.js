f1def18137ce76a5b2a972a8c35f9c67893c2529
IA <IA@MacBook-Pro-de-Elsa.local>
2020-02-06 22:07:21 +0100

    Adding a button to make tasks private : - Add task.setPrivate method - Update renderTasks   to pass in showPrivateButton - Add private button, shown only to owner - Add private button event handler to Task

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -40,11 +40,19 @@ class App extends Component {
         let filteredTasks = this.props.tasks;
         if (this.state.hideCompleted) {
             filteredTasks = filteredTasks.filter(task => !task.checked);
-
         }
-        return filteredTasks.map((task) => (
-            <Task key={task._id} task={task} />
-        ));
+        return filteredTasks.map((task) => {
+            const currentUserId = this.props.currentUser && this.props.currentUser._id;
+            const showPrivateButton = task.owner === currentUserId;
+
+            return (
+                <Task 
+                    key={task._id}
+                    task={task}
+                    showPrivateButton={showPrivateButton}
+                />
+            );
+        });
     }
 
     render() {
